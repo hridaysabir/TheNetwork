6c799a52ff9a69c71000720bfe4bb56c036a0756
hsabir <sabir.h@northeastern.edu>
2021-12-11 14:29:50 -0500

    menubar styling on link

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
 import { Menu } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 function MenuBar() {
-  const [activeItem, setActiveItem] = useState('');
+  const pathName = window.location.pathname;
+  const path = pathName === '/' ? 'home' : pathName.substr(1)
+  const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem( name )
 
     return (
-      <Menu>
+      <Menu color='teal'>
         <Menu.Item
           name='home'
           active={activeItem === 'home'}
           onClick={handleItemClick}
+          as={Link}
+          to="/"
         >
           Home
         </Menu.Item>
@@ -20,6 +25,8 @@ function MenuBar() {
           name='login'
           active={activeItem === 'login'}
           onClick={handleItemClick}
+          as={Link}
+          to="/login"
         >
           Login
         </Menu.Item>
@@ -28,6 +35,8 @@ function MenuBar() {
           name='register'
           active={activeItem === 'register'}
           onClick={handleItemClick}
+          as={Link}
+          to="/register"
         >
           Register
         </Menu.Item>
